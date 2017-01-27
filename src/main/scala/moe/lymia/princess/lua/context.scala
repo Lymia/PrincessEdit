9f7c17c8b11843c6b5dcdb7976cf374a40cc5b94
Lymia Aluysia <lymia@lymiahugs.com>
2017-01-26 23:52:06 -0600

    Add Scala wrapper around Jill.

---
+++ src/main/scala/moe/lymia/princess/lua/context.scala
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2017 Lymia Alusyia <lymia@lymiahugs.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package moe.lymia.princess.lua
+import java.io.{InputStream, Reader}
+import java.util
+
+import scala.collection.JavaConverters._
+
+case class LuaRegistryEntry[T]()
+final case class LuaState(L: Lua) extends AnyVal {
+  def getRegistry[T](entry: LuaRegistryEntry[T], default: => T) = {
+    val reg = L.getRegistry
+    if(!reg.contains(entry)) reg.putlua(L, reg, default)
+    reg.getlua(entry).asInstanceOf[T]
+  }
+
+  def isMain: Boolean = L.isMain
+  def status: Int = L.status()
+
+  // stack manipulation functions
+  def getTop: Int = L.getTop
+  def setTop(n: Int): Unit = L.setTop(n)
+
+  def pop(n: Int): Unit = L.pop(n)
+  def popTop() = {
+    val top = L.value(L.getTop).returnWrapper
+    L.pop(1)
+    top
+  }
+  def push[T : ToLua](o: T): Unit = L.push(o.toLua(this))
+  def pushValue(idx: Int): Unit = L.pushValue(idx)
+  def insert[T : ToLua](o: T, idx: Int): Unit = L.insert(o.toLua(this), idx)
+
+  def xmove(to: LuaState, n: Int): Unit = L.xmove(to.L, n)
+
+  def value(idx: Int) = L.value(idx).returnWrapper
+
+  // conversion functions
+  def isNoneOrNil(narg: Int): Boolean = L.isNoneOrNil(narg)
+  def toBoolean[O : ToLua](o: O): Boolean = L.toBoolean(o.toLua(this))
+  def toInteger[O : ToLua](o: O): Int = L.toInteger(o.toLua(this))
+  def toNumber[O : ToLua](o: O): Double = L.toNumber(o.toLua(this))
+  def toString[O : ToLua](o: O): String = L.toString(o.toLua(this))
+  def toThread[O : ToLua](o: O): Lua = L.toThread(o.toLua(this))
+  def toUserdata[O : ToLua](o: O): LuaUserdata = L.toUserdata(o.toLua(this))
+
+  // field manipulation functions
+  def getGlobals: LuaTable = L.getGlobals
+  def getRegistry: LuaTable = L.getRegistry
+
+  def getGlobal(name: String) = L.getGlobal(name).returnWrapper
+  def setGlobal[V : ToLua](name: String, value: V): Unit = L.setGlobal(name, value.toLua(this))
+
+  def getMetafield[O : ToLua](o: scala.Any, event: String) = L.getMetafield(o.toLua(this), event).returnWrapper
+
+  def getTable[T : ToLua, K : ToLua](t: T, k: K) = L.getTable(t.toLua(this), k.toLua(this)).returnWrapper
+  def setTable[T : ToLua, K : ToLua, V : ToLua](t: T, k: K, v: V) =
+    L.setTable(t.toLua(this), k.toLua(this), v.toLua(this))
+  def setField[T : ToLua, V : ToLua](t: T, name: String, v: V): Unit = L.setField(t.toLua(this), name, v.toLua(this))
+
+  def rawGet[T : ToLua, K : ToLua](t: T, k: K) = Lua.rawGet(t.toLua(this), k.toLua(this)).returnWrapper
+  def rawSet[T : ToLua, K : ToLua, V : ToLua](t: T, k: K, v: V) = L.rawSet(t.toLua(this), k.toLua(this), v.toLua(this))
+
+  // chunk loading
+  private def popLoad(status: Int) = {
+    val ret = if(status == 0) Left (L.value(L.getTop).fromLua[LuaClosure](this))
+              else            Right(L.value(L.getTop).fromLua[String    ](this))
+    L.pop(1)
+    ret
+  }
+  def load(in: InputStream, chunkname: String) = popLoad(L.load(in, chunkname))
+  def load(in: Reader, chunkname: String) = popLoad(L.load(in, chunkname))
+  def loadFile(filename: String) = popLoad(L.loadFile(filename))
+  def loadString(s: String, chunkname: String) = popLoad(L.loadString(s, chunkname))
+  def doString(s: String): Int = L.doString(s)
+
+  // call functions
+  def call(nargs: Int, nresults: Int) = L.call(nargs, nresults)
+  def pcall(nargs: Int, nresults: Int, ef: LuaClosure): Int = L.pcall(nargs, nresults, ef.toLua(this))
+  def callMeta(obj: Int, event: String): Boolean = L.callMeta(obj, event)
+
+  // api functions
+  def error(message: String) = {
+    L.error(message.toLua(this))
+    sys.error("L.error returned unexpectedly!")
+  }
+  def gc(what: Int, data: Int): Int = L.gc(what, data)
+
+  def where(level: Int): String = L.where(level)
+  def setHook(mask: Int, count: Int)(fn: (LuaState, Debug) => Unit) =
+    L.setHook(new Hook {
+      override def luaHook(L: Lua, ar: Debug) = {
+        fn(new LuaState(L), ar)
+        0
+      }
+    }, mask, count)
+
+  def getFenv[T : ToLua](o: T): LuaTable = L.getFenv(o.toLua(this))
+  def setFenv[O : ToLua, T : ToLua](o: O, table: T): Boolean = L.setFenv(o.toLua(this), table.toLua(this))
+
+  def getMetatable[T : ToLua](o: T): LuaTable = L.getMetatable(o.toLua(this))
+  def setMetatable[O : ToLua, M : ToLua](o: O, mt: M): Unit = L.setMetatable(o.toLua(this), mt.toLua(this))
+
+  // operator functions
+  def concat(n: Int): Unit = L.concat(n)
+  def equal[A : ToLua, B: ToLua](o1: A, o2: B): Boolean = L.equal(o1.toLua(this), o2.toLua(this))
+  def lessThan[A : ToLua, B : ToLua](o1: A, o2: B): Boolean = L.lessThan(o1.toLua(this), o2.toLua(this))
+  def tableKeys[T : ToLua](t: T) = L.tableKeys(t.toLua(this)).asScala
+  def `type`(idx: Int): Int = L.`type`(idx)
+  def typeNameOfIndex(idx: Int): String = L.typeNameOfIndex(idx)
+}
+object LuaState {
+  def makeBasicContext() = {
+    val L = new Lua()
+
+    BaseLib.open(L)
+    MathLib.open(L)
+    OSLib.open(L)
+    StringLib.open(L)
+    TableLib.open(L)
+
+    new LuaState(L)
+  }
+  def makeFullContext() = {
+    val L = makeBasicContext()
+    PackageLib.open(L.L)
+    L
+  }
+  def makeSafeContext() = {
+    val L = makeBasicContext()
+    new SafePackageLib().open(L)
+    L.setGlobal("loadfile", LuaNil)
+    L
+  }
+}
---
+++ src/main/scala/moe/lymia/princess/lua/dsl.scala
@@ -0,0 +1,224 @@
+/*
+ * Copyright (c) 2017 Lymia Alusyia <lymia@lymiahugs.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package moe.lymia.princess.lua
+
+import scala.collection.mutable
+import scala.collection.JavaConverters._
+import scala.reflect.ClassTag
+
+import scala.language.implicitConversions
+
+class LuaUserdataType[T : ClassTag] {
+  private val metatableInitializers = new mutable.ArrayBuffer[(LuaState, LuaTable) => Unit]
+  protected def registerInit(fn: (LuaState, LuaTable) => Unit) = metatableInitializers.append(fn)
+  def tag = implicitly[ClassTag[T]]
+  def getMetatable(L: LuaState) = {
+    if(metatableInitializers.isEmpty) None
+    else {
+      val table = new LuaTable()
+      metatableInitializers.foreach(_(L, table))
+      Some(table)
+    }
+  }
+}
+
+case class LuaThreadWrapper(thread: Lua)
+case class LuaExecWrapper(fn: LuaState => Any)
+class LuaOutObject(val wrapped: Any) extends AnyVal {
+  def toLua(L: LuaState) = wrapped match {
+    case LuaExecWrapper(fn) => fn(L)
+    case any => any
+  }
+}
+sealed trait ToLua[T] {
+  def toLua(t: T): LuaOutObject
+}
+sealed trait FromLua[T] {
+  def fromLua(L: Lua, v: Any, source: String): T
+}
+sealed trait LuaParameter[T] extends ToLua[T] with FromLua[T]
+
+case class LuaClosure(fn: Any)
+case class ScalaLuaClosure(fn: LuaState => Seq[LuaOutObject]) extends AnyVal
+
+class LuaReturnWrapper(val wrapped: Any) extends AnyVal {
+  def as[T : FromLua](L: LuaState) = wrapped.fromLua[T](L)
+}
+
+case object LuaNil
+
+trait LuaImplicits extends LuaGeneratedImplicits {
+  private def typerror[T](L: Lua, source: String, got: String, expected: String): T = {
+    L.error(s"bad argument $source ($expected expected, got $got)")
+    sys.error("L.error returned unexpectedly!")
+  }
+  private def typerror[T](L: Lua, source: String, got: Any, expected: Int): T =
+    typerror(L, source, Lua.typeName(Lua.`type`(expected)), Lua.typeName(expected))
+
+  // Function wrapper
+  implicit def unitClosure2luaClosure(fn: LuaState => Unit): ScalaLuaClosure = ScalaLuaClosure(L => { fn(L); Seq() })
+  implicit def closure2luaClosure(fn: LuaState => Seq[LuaOutObject]): ScalaLuaClosure = ScalaLuaClosure(fn)
+  implicit def nullUnitClosure2luaClosure(fn: () => Unit): ScalaLuaClosure = ScalaLuaClosure(L => { fn(); Seq() })
+  implicit def nullClosure2luaClosure(fn: () => Seq[LuaOutObject]): ScalaLuaClosure = ScalaLuaClosure(L => { fn() })
+
+  private class LuaClosureWrapper(fn: ScalaLuaClosure) extends LuaJavaCallback {
+    override def luaFunction(Lr: Lua) = {
+      val L = new LuaState(Lr)
+      val rets = fn.fn(L)
+      for(ret <- rets) Lr.push(ret.toLua(L))
+      rets.length
+    }
+  }
+  implicit def luaClosure2LuaFunction(fn: ScalaLuaClosure): LuaClosure = LuaClosure(new LuaClosureWrapper(fn))
+
+  // Lua Object wrappers
+  implicit def toLua2luaObject[T : ToLua](obj: T): LuaOutObject = implicitly[ToLua[T]].toLua(obj)
+  implicit class FromLuaAnyExtension(obj: Any) {
+    def fromLua[T : FromLua](L: LuaState, source: String = "<java function>") =
+      implicitly[FromLua[T]].fromLua(L.L, obj, source)
+    private[lua] def returnWrapper = new LuaReturnWrapper(obj)
+  }
+
+  implicit object LuaParameterAny extends LuaParameter[Any] {
+    override def toLua(t: Any): LuaOutObject = new LuaOutObject(t)
+    override def fromLua(L: Lua, v: Any, source: String) = v
+  }
+  implicit object LuaParameterLuaNil extends ToLua[LuaNil.type] {
+    override def toLua(t: LuaNil.type): LuaOutObject = new LuaOutObject(Lua.NIL)
+  }
+
+  private class LuaParameterNumeric[N : Numeric](toN: Double => N) extends LuaParameter[N] {
+    override def toLua(n: N) = new LuaOutObject(implicitly[Numeric[N]].toDouble(n))
+    override def fromLua(L: Lua, v: Any, source: String) = v match {
+      case n: java.lang.Double => toN(n)
+      case _ => typerror(L, source, v, Lua.TNUMBER)
+    }
+  }
+  implicit val LuaParameterByte   : LuaParameter[Byte  ] = new LuaParameterNumeric[Byte  ](_.toByte)
+  implicit val LuaParameterShort  : LuaParameter[Short ] = new LuaParameterNumeric[Short ](_.toShort)
+  implicit val LuaParameterInt    : LuaParameter[Int   ] = new LuaParameterNumeric[Int   ](_.toInt)
+  implicit val LuaParameterLong   : LuaParameter[Long  ] = new LuaParameterNumeric[Long  ](_.toLong)
+  implicit val LuaParameterFloat  : LuaParameter[Float ] = new LuaParameterNumeric[Float ](_.toFloat)
+  implicit val LuaParameterDouble : LuaParameter[Double] = new LuaParameterNumeric[Double](identity)
+
+  implicit object LuaParameterBoolean extends LuaParameter[Boolean] {
+    override def toLua(b: Boolean) = new LuaOutObject(b)
+    override def fromLua(L: Lua, v: Any, source: String) = L.toBoolean(v)
+  }
+  implicit object LuaParameterTable extends LuaParameter[LuaTable] {
+    def toLua(t: LuaTable) = new LuaOutObject(t)
+    override def fromLua(L: Lua, v: Any, source: String) = v match {
+      case n: LuaTable => n
+      case _ => typerror(L, source, v, Lua.TTABLE)
+    }
+  }
+  implicit object LuaParameterThread extends LuaParameter[Lua] {
+    def toLua(t: Lua) = new LuaOutObject(t)
+    override def fromLua(L: Lua, v: Any, source: String) = v match {
+      case n: Lua => n
+      case _ => typerror(L, source, v, Lua.TTHREAD)
+    }
+  }
+
+  implicit object LuaParameterString extends LuaParameter[String] {
+    override def toLua(t: String) = new LuaOutObject(t)
+    override def fromLua(L: Lua, v: Any, source: String) = L.toString(v) match {
+      case null => typerror(L, source, v, Lua.TSTRING)
+      case s => s
+    }
+  }
+
+  private val userdataMetatableCache = new mutable.WeakHashMap[LuaUserdataType[_], Any]
+  private type MetatableCacheType = mutable.HashMap[LuaUserdataType[_], Option[LuaTable]]
+  private val metatableCache = LuaRegistryEntry[MetatableCacheType]()
+  implicit def luaParameterUnwrapUserdata[T : LuaUserdataType] = new LuaParameter[T] {
+    private def metadata = implicitly[LuaUserdataType[T]]
+    override def toLua(t: T) = new LuaOutObject(LuaExecWrapper { L =>
+      val cache = L.getRegistry(metatableCache, new MetatableCacheType)
+      val mt = cache.getOrElseUpdate(metadata, metadata.getMetatable(L))
+      val ud = new LuaUserdata(t)
+      mt.foreach(ud.setMetatable)
+      ud
+    })
+    override def fromLua(L: Lua, v: Any, source: String) = v match {
+      case v: LuaUserdata =>
+        if(!metadata.tag.runtimeClass.isAssignableFrom(v.getClass))
+          typerror(L, source, v.getClass.toString, metadata.tag.toString)
+        v.asInstanceOf[T]
+      case _ => typerror(L, source, v, Lua.TUSERDATA)
+    }
+  }
+
+  implicit object LuaParameterLuaClosure extends LuaParameter[LuaClosure] {
+    override def toLua(t: LuaClosure) = new LuaOutObject(t.fn)
+    override def fromLua(L: Lua, v: Any, source: String) = v match {
+      case v: LuaFunction     => LuaClosure(v)
+      case v: LuaJavaCallback => LuaClosure(v)
+      case _ => typerror(L, source, v, Lua.TFUNCTION)
+    }
+  }
+
+  // Scala type wrappers
+  implicit def toLuaSeq[V : ToLua] = new ToLua[Seq[V]] {
+    override def toLua(s: Seq[V]) = new LuaOutObject(LuaExecWrapper { L =>
+      val t = new LuaTable()
+      for((v, k) <- s.zipWithIndex) t.putnum(k + 1, implicitly[ToLua[V]].toLua(v))
+      t
+    })
+  }
+  implicit def fromLuaSeq[V : FromLua] = new FromLua[Seq[V]] {
+    override def fromLua(L: Lua, v: Any, source: String): Seq[V] = v match {
+      case table: LuaTable =>
+        for(k <- table.array.take(table.sizeArray)) yield
+          implicitly[FromLua[V]].fromLua(L, table.getlua(k), s"$source in table value")
+      case _ => typerror(L, source, v, Lua.TTABLE)
+    }
+  }
+
+  implicit def toLuaMap[K : ToLua, V : ToLua] = new ToLua[Map[K, V]] {
+    override def toLua(m: Map[K, V]) = new LuaOutObject(LuaExecWrapper { L =>
+      val t = new LuaTable()
+      for((k, v) <- m) t.putlua(L.L, implicitly[ToLua[K]].toLua(k), implicitly[ToLua[V]].toLua(v))
+      t
+    })
+  }
+  implicit def fromLuaMap[K : FromLua, V : FromLua] = new FromLua[Map[K, V]] {
+    override def fromLua(L: Lua, v: Any, source: String): Map[K, V] = v match {
+      case table: LuaTable =>
+        (for(k <- table.keySet().asScala) yield (
+          implicitly[FromLua[K]].fromLua(L, k              , s"$source in table key"  ),
+          implicitly[FromLua[V]].fromLua(L, table.getlua(k), s"$source in table value"))).toMap
+      case _ => typerror(L, source, v, Lua.TTABLE)
+    }
+  }
+
+  implicit def toLuaOptional[T : ToLua] = new ToLua[Option[T]] {
+    override def toLua(t: Option[T]) = t.fold(new LuaOutObject(Lua.NIL))(implicitly[ToLua[T]].toLua)
+  }
+  implicit def fromLuaOptional[T : FromLua] = new FromLua[Option[T]] {
+    override def fromLua(L: Lua, v: Any, source: String) = Lua.`type`(v) match {
+      case Lua.TNIL | Lua.TNONE => None
+      case _ => Some(implicitly[FromLua[T]].fromLua(L, v, source))
+    }
+  }
+}
